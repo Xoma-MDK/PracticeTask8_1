dae47388cabe6cb316d9033345cd0d1494ee3d24
Daniil Molotkov <molotkov.58@ya.ru>
2021-11-22 11:23:10 +0700

    Добавьте файлы проекта.

---
+++ PracticeTask8_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PracticeTask8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            string Buffer = "";
+            while (N != 0)
+            {
+                Buffer += Convert.ToString(N%10);
+                N = N / 10;
+            }
+            Console.WriteLine(Convert.ToInt32(Buffer));
+            Console.Write("Введите число: ");
+            N = Convert.ToInt32(Console.ReadLine());
+            Buffer = "";
+            for (int i = 0; i <= N; i++)
+            {
+                Buffer += Convert.ToString(N % 10);
+                N = N / 10;
+            }
+            Console.WriteLine(Convert.ToInt32(Buffer));
+        }
+    }
+}
